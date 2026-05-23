70459c7e45013971c1fe02306f925920f2a79db2
Sumit Prajapati <162676130+sumit7444@users.noreply.github.com>
2026-05-23 21:13:00 +0530

    Update pallindrome.py

--- pallindrome.py
+++ pallindrome.py
@@ -6,5 +6,5 @@ for num in range(1,1001):
         digit=temp%10
         reverse_num=reverse_num*10+digit
         temp=temp//10
-    if num==reverse_num,
+    if num==reverse_num:
         print(num)
